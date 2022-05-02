4155379e729ec0e05056e37aaaf2829dd27e69d2
Sandeep <nanisandeep876@gmail.com>
2022-05-03 00:07:20 +0530

    1 &2

---
+++ Day1.py
@@ -0,0 +1,13 @@
+a = 'This is Sandeep'
+
+b='and i am starting 100 days python challenge today'
+
+c=a +' '+b
+
+print(c)
+print(c.upper())
+print(c.lower())
+print(c.split(","))
+print(c[:20])
+print(c[-1])
+print(c[8:17])
---
+++ Day2.py
@@ -0,0 +1,20 @@
+
+name='sid'
+a='variable is created and we assign a value to it.'
+num=type(a)
+Num=type(37)
+b,c,d='data type','var','int'
+p=['this is the 2nd day of 100 days coding challenge','type can change after they have set' ]
+x,y=p
+
+print(name)
+print(a)
+print(num)
+print(Num)
+print(b)
+print(c)
+print(d)
+print(x)
+print(y)
+print(p[0])
+print(p[1])
