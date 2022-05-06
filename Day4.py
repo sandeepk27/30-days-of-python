f7676c5ed902bed0ad6ef4178a3c83671d9fda02
Sandeep <nanisandeep876@gmail.com>
2022-05-06 14:15:38 +0530

    5

---
+++ Day4.py
@@ -0,0 +1,24 @@
+x=70
+y=130
+
+z=x+y
+x+=y 
+i=y%5
+print('Answer is:')
+print(z,' ',x,' ',i)
+
+
+if x>y:
+    print('This is correct answer')
+else:
+    print('Not correct answer')
+
+MBA=['finance','HR','Marketing']
+MCA=['cs','CA']
+print('cs' in MCA)
+print(MCA is MBA)
+print(x>100 and y<120)
+x&=25
+print(x)
+x^=2
+print(x)
