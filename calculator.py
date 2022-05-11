2bf2ad15042e21a7c10462c05780a1d73afd0156
Sandeep <87967156+sandeepk27@users.noreply.github.com>
2022-05-11 23:31:02 +0530

    calculator

--- calculator.py
+++ calculator.py
@@ -3,26 +3,22 @@ import tkinter.font as font
 
 
 root=Tk()
-root.geometry("400x440")
+root.geometry("400x460")
 root.title("Calculator")
 root.resizable(0, 0)
 
-#Creating a StringVar to take
+# Creating a StringVar to take
 input=StringVar ()
 myFont=font. Font(size=15)
 
 
-#Creating an Entry widget to get the
-screen=Entry(root, text=input, width=30,
-            justify='right', font=(10), bd=4)
+#C reating an Entry widget to get the
+screen=Entry(root, text=input, width=30,justify='right', font=(10), bd=4)
 
-#we will usergrid like structure
-
-screen.grid(row=0, columnspan=4, padx=15,
-            pady=15, ipady=5)
+# we will usergrid like structure
+screen.grid(row=0, columnspan=4, padx=15,pady=15, ipady=5)
 
 #Key matrix contains all the required the keys
-
 key_matrix =[["c", u"\u221A", "/", "<-"],
             ["7", "8", "9", "*"],
             ["4", "5", "6","-"],
@@ -62,14 +58,14 @@ def Calculate(event):
         elif button=="-": #Showing The Results
             #Calculating the mathematical exp. using eval
             ans_to_print=str(eval(input.get()))
-        #You may add many more operations
+        # You may add many more operations
         else:
-        #Displaying the digit pressed on screen
+        # Displaying the digit pressed on screen
             input.set(input.get()+str(button))
     except:
-        #In case invalid syntax given in expression
+        # In case invalid syntax given in expression
         input.set("wrong Operation")
-#Number of rows containing buttons
+# Number of rows containing buttons
 for i in range(len(key_matrix)):
     # Number of columns
     for j in range(len(key_matrix[i])):
@@ -83,5 +79,5 @@ for i in range(len(key_matrix)):
         # Assigning an action to the buttons
         btn_dict["btn_"+str(key_matrix[i][j])].bind('<Button-1>', Calculate)
 
-#Running the main loop
+# Running the main loop
 root.mainloop()
