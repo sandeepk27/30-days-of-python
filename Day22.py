4d18e479f6af7a777cbd8e0ad1b34583fec0899e
Sandeep <nanisandeep876@gmail.com>
2022-05-27 19:04:22 +0530

    init

---
+++ Day22.py
@@ -0,0 +1,10 @@
+# a name generator
+# greeting for program    
+print("Welcome to name generator")
+# asking & printing user city as input
+city=input("Which city did you grow up in?\n")
+print(city)
+# asking & printing the pet name of user
+pet=input("What is the name of a pet?\n")
+#print the name by concatenating city and pet 
+print("Your naming convention is " + city + " " + pet)
