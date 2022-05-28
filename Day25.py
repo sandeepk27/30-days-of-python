06882053f28ff905a1214b38217cb185ebcd1771
Sandeep <nanisandeep876@gmail.com>
2022-05-28 23:21:10 +0530

    init

---
+++ Day25.py
@@ -0,0 +1,85 @@
+print("Welcome to pizza deliveries!")
+size=input("Select the size you want to order? S, M, or L")
+extra_pepper=input("Do you want pepperoni? Y or N")
+cheese=input("Do you want extra cheese? Y or N")
+
+total_bill=0
+if size =="S":
+    total_bill +=15
+elif size=="M":
+    total_bill +=20
+else:
+    total_bill+=25
+
+    
+if extra_pepper=="Y":
+    if size=="S":
+        total_bill +=2
+    else:
+        total_bill +=3
+        
+if cheese=="Y":
+    total_bill +=1
+
+print(f"Your final bill is ${total_bill}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('''
+*******************************************************************************
+            |                   |                  |                     |
+________    |_______________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+            |                `"=._o`"=._      _`"=._                     |
+_________   |_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+        |             |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+    ____|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#Write your code below this line 👇
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+# we using \ for escape the character here
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
